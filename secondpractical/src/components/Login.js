f6ea81e03e4aa0912f363c80461ebed14ad26ed4
Nipun Dilshan <nipundilshan991@gmail.com>
2024-11-30 11:24:43 +0530

    ui

--- secondpractical/src/components/Login.js
+++ secondpractical/src/components/Login.js
@@ -10,4 +10,33 @@ export default function Login(){
             ...prevUser,[name]:value,
         }))
     }
+    const aunthentication=()=>{
+        if(user.username==="Admin" && user.pwd === "abc@123"){
+            setUser({ username: "", pwd: "", error:""})
+            navigate('/dash');
+        }
+        else{
+            setUser({'error':"Please check your username and password"})
+        }
+    }
+
+   return(
+    <div>
+        <h1>Login here</h1>
+        <table>
+            <tr>
+                <td>Username:</td>
+                <td><input type="text" name="username" onChange={getTextInput}/></td>
+            </tr>
+            <tr>
+                <td>password</td>
+                <td><input type="password" name="pwd" onChange={getTextInput}/></td>
+            </tr>
+            <tr>
+                <td style={{color:'red'}} colSpan={2}>{user.error}</td>
+            </tr>
+        </table>
+        <button onClick={aunthentication}>Login</button>
+    </div>
+   )
 }
