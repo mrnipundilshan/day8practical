885f4d5664d78564d4b03a2278a7e5115749b05c
Nipun Dilshan <nipundilshan991@gmail.com>
2024-11-30 10:30:22 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Router>
+        <nav>
+          <ul>
+            <li><Link to='/home'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+          </ul>
+        </nav>
         <Routes>
          
         </Routes>
