81173445c3b8607e3e9c794eaf44e72554f94fc5
Nipun Dilshan <nipundilshan991@gmail.com>
2024-11-30 11:49:24 +0530

    finished

--- secondpractical/src/App.js
+++ secondpractical/src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 
 function App() {
   return (
-    <>
-      
-    </>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/dash' element={<Dashboard/>}/>
+        </Routes>
+      </Router>
   );
 }
 
--- secondpractical/src/components/Login.js
+++ secondpractical/src/components/Login.js
@@ -1,42 +1,43 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function Login(){
+export default function Login() {
     const navigate = useNavigate();
-    const [user, setUser] = useState({username:"",pwd:"",error:""})
-    const getTextInput=(e)=>{
-        const {name,value}=e.target;
-        setUser((prevUser)=>({
-            ...prevUser,[name]:value,
+    const [user, setUser] = useState({ username: "", pwd: "", error: "" })
+    const getTextInput = (e) => {
+        const { name, value } = e.target;
+        setUser((prevUser) => ({
+            ...prevUser, [name]: value,
         }))
     }
-    const aunthentication=()=>{
-        if(user.username==="Admin" && user.pwd === "abc@123"){
-            setUser({ username: "", pwd: "", error:""})
+
+    const aunthentication = () => {
+        if (user.username === "Admin" && user.pwd === "abc@123") {
+            setUser({ username: "", pwd: "", error: "" })
             navigate('/dash');
         }
-        else{
-            setUser({'error':"Please check your username and password"})
+        else {
+            setUser({ 'error': "Please check your username and password" })
         }
     }
 
-   return(
-    <div>
-        <h1>Login here</h1>
-        <table>
-            <tr>
-                <td>Username:</td>
-                <td><input type="text" name="username" onChange={getTextInput}/></td>
-            </tr>
-            <tr>
-                <td>password</td>
-                <td><input type="password" name="pwd" onChange={getTextInput}/></td>
-            </tr>
-            <tr>
-                <td style={{color:'red'}} colSpan={2}>{user.error}</td>
-            </tr>
-        </table>
-        <button onClick={aunthentication}>Login</button>
-    </div>
-   )
+    return (
+        <div>
+            <h1>Login here</h1>
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td><input type="text" name="username" onChange={getTextInput} /></td>
+                </tr>
+                <tr>
+                    <td>password:</td>
+                    <td><input type="password" name="pwd" onChange={getTextInput} /></td>
+                </tr>
+                <tr>
+                    <td style={{ color: 'red' }} colSpan={2}>{user.error}</td>
+                </tr>
+            </table>
+            <button onClick={aunthentication}>Login</button>
+        </div>
+    );
 }
